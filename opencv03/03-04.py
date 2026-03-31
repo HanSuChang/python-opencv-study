85c63602fa47a057dde4046a5aedba66c9108950
HanSuChang <hsc0724321@gmail.com>
2026-03-31 10:02:18 +0900

    크기변환(Resize)

--- opencv03/03-04.py
+++ opencv03/03-04.py
@@ -1,3 +1,11 @@
+# 크기 변환
+### 크기 변환 (Resize)
+
+#함수: `cv2.resize(src, dsize, fx, fy, interpolation)`
+#보간법(Interpolation):
+#   - `cv2.INTER_LINEAR`: 기본값, 속도 빠름. 주변 픽셀을 가중 평균내기.
+#    - `cv2.INTER_CUBIC`: 화질 좋음, 속도 느림. (확대 시 추천). 주변 픽셀을 3차 함수로 계산해서 보간하기
+#    - `cv2.INTER_AREA`: 영역 보간, 축소 시 물결 무늬(Moire) 방지에 좋음. 가까운 픽셀과 최대한 비슷하게 (가까운 픽셀을 복사하기)
 import cv2
 
 
